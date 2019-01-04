da6baf542361f4b58c80a59d250fcde73e467ea5
slmoth <sdmatthews01@gmail.com>
2019-01-04 17:25:33 -0500

    changes
    passing data from a component ~ halfway thru - rewatch

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -1,12 +1,38 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { IProduct } from './product';
+
 
 @Component({
   selector: 'pm-products',
-  templateUrl: './product-list.component.html'
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
+export class ProductListComponent 
+                implements OnInit {
   pageTitle: string = 'Product List';
-  products: any[] = [
+  imageWidth: number = 50;
+  imageMargin: number = 2;
+  showImage: boolean = false;
+
+  _listFilter: string;
+  get listFilter(): string {
+    return this._listFilter;
+  }
+  set listFilter(value: string) {
+    this._listFilter = value;
+    this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+  }
+
+  performFilter(filterBy: string): IProduct[] {
+    filterBy = filterBy.toLocaleLowerCase();
+
+    return this.products.filter((product:IProduct) =>
+            product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+  }
+
+  filteredProducts: IProduct[];
+
+  products: IProduct[] = [
     {
       "productId": 1,
       "productName": "Leaf Rake",
@@ -27,4 +53,21 @@ export class ProductListComponent {
       "starRating": 4.2,
       "imageUrl": "https://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
     }];
+
+  constructor() {
+    this.filteredProducts = this.products;
+    this.listFilter = "";
+  }
+
+  onRatingClicked(message: string): void {
+    this.pageTitle = 'Product List: ' + message;
+  }
+
+  toggleImage(): void {
+    this.showImage = !this.showImage;
+  }
+
+  ngOnInit(): void {
+    console.log('In OnInit');
+  }
 }
