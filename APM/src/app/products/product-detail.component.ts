b9f8a2d7a81cbc700d363b406c5f8ef0c66fc0bd
slmoth <sdmatthews01@gmail.com>
2019-01-14 17:16:19 -0500

    module 12 protecting routes with guards

--- APM/src/app/products/product-detail.component.ts
+++ APM/src/app/products/product-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -9,9 +11,28 @@ export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Detail';
   product: IProduct;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private router: Router) {
+    console.log(this.route.snapshot.paramMap.get('id'));
+    
+   }
 
   ngOnInit() {
+    let id = +this.route.snapshot.paramMap.get('id');
+    this.pageTitle += `: ${id}`;
+    this.product = {
+      'productId': id,
+      'productName': 'Leaf Rake',
+      'productCode': 'GDN-0011',
+      'releaseDate': 'March 19, 2016',
+      'description': 'Leaf rate with 48-inch wooden handel.',
+      'price': 19.95,
+      'starRating': 3.2,
+      'imageUrl': 'https://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png'
+    }
+  }
+
+  onBack(): void {
+    this.router.navigate(['/products']);
   }
 
 }
